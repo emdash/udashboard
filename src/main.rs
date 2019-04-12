949496bff2195819a14a26ef12f1e8926859ab4b
Brandon Lewis <dotsony@gmail.com>
2019-04-12 14:36:40 -0700

    Rework

--- src/main.rs
+++ src/main.rs
@@ -3,43 +3,62 @@ extern crate ron;
 
 use ron::de::from_reader;
 use std::{fs::File};
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize};
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Divisions {
-    major: u32,
-    minor: u32
+#[derive(Deserialize, Debug)]
+pub enum Divisions {
+    None,
+    Uniform(f32),
+    MajorMinor(f32, f32),
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Precision {
-    integer: u32,
-    decimal: u32
+#[derive(Deserialize, Debug)]
+pub enum Format {
+    Numeric(u32, u32),
+    Custom(String)
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub enum TickStyle {
-    NoTicks,
-    Numbered(Divisions),
-    Unumbered(Divisions)
+#[derive(Deserialize, Debug)]
+pub struct Range(f32, f32);
+
+#[derive(Deserialize, Debug)]
+pub enum GaugeStyle {
+    IndicatorOnly,
+    Outline,
+    Filled,
+    Dashed
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Unit(String);
-
-#[derive(Serialize, Deserialize, Debug)]
-pub enum Interval {
-    Open,
-    LowerBound(f32),
-    UpperBound(f32),
-    Range(f32, f32)
+#[derive(Deserialize, Debug)]
+pub enum Lamp {
+    Round,
+    Rect,
+    RoundedRect,
+    Image(String)
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Deserialize, Debug)]
+pub enum Unit {
+    None,
+    Named(String),
+}
+
+#[derive(Deserialize, Debug)]
+pub enum Test {
+    Always,
+    Never,
+    LessThan(f32),
+    GreaterThan(f32),
+    Equal(f32),
+    Between(f32, f32)
+}
+
+#[derive(Deserialize, Debug)]
 pub enum Color {
     Clear,
     Black,
     White,
+    Grey,
     Red,
     Orange,
     Yellow,
@@ -47,120 +66,158 @@ pub enum Color {
     Blue,
     Indigo,
     Violet,
-    RGBA(f32, f32, f32, f32),
+    RGBA(f32, f32, f32, f32)
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Style {
-    background: Color,
-    foreground: Color,
-    indicator: Color,
-    warning: Color,
-    danger: Color
+#[derive(Deserialize, Debug)]
+pub enum Pattern {
+    Inherit,
+    Solid(Color),
+    SlowBlink(Color),
+    FastBlink(Color)
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Deserialize, Debug)]
+pub struct StyleDef {
+    name: String,
+    background: Pattern,
+    foreground: Pattern,
+    indicator: Pattern
+}
+
+#[derive(Deserialize, Debug)]
+pub enum Style {
+    Default(Color, Color, Color),
+    Define(StyleDef)
+}
+
+#[derive(Deserialize, Debug)]
+pub enum State {
+    Default,
+    Alarm(String)
+}
+
+#[derive(Deserialize, Debug)]
 pub enum GaugeType {
-    Numeric(Precision),
-    Dial(TickStyle),
-    SemiDial(TickStyle),
-    Bar(TickStyle),
-    Triangle(TickStyle)
+    Dial(Range, Divisions, GaugeStyle),
+    VerticalBar(Range, Divisions, GaugeStyle),
+    HorizontalBar(Range, Divisions, GaugeStyle),
+    VerticalWedge(Range, Divisions, GaugeStyle),
+    HorizontalWedge(Range, Divisions, GaugeStyle),
+    IdiotLight(Lamp),
+    Text(Format, GaugeStyle),
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct ChannelName(String);
+#[derive(Deserialize, Debug)]
+pub struct When(String, Test, State);
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Polynomial {
-    coeffs: Vec<f32>
+#[derive(Deserialize, Debug)]
+enum Source {
+    Static(f32),
+    Oscillating(f32, f32),
+    Random(f32, f32),
+    Channel(String),
 }
 
-impl Polynomial {
-    fn eval(&self, x: f32) -> f32 {
-        let mut accum: f32 = 0.0;
-        let mut value: f32 = 1.0;
-
-        for c in self.coeffs.iter() {
-            accum += c * value;
-            value *= x;
-        }
-
-        accum
-    }
+#[derive(Deserialize, Debug)]
+pub enum Conversion {
+    Identity,
+    Scale(f32),
+    Linear(f32, f32),
+    Polynomial(Vec<f32>)
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Deserialize, Debug)]
 pub enum Length {
     Pixel(f32),
     Percent(f32)
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct AbsolutePosition {
+#[derive(Deserialize, Debug)]
+pub enum Size {
+    Radius(Length),
+    Box(Length, Length)
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Point {
     x: Length,
     y: Length
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Deserialize, Debug)]
 pub struct GridSize(u32, u32);
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Deserialize, Debug)]
 pub struct GridPosition(u32, u32);
 
-#[derive(Serialize, Deserialize, Debug)]
-pub enum AbsoluteSize {
-    FromCenter(Length),
-    Square(Length),
-    Rect(Length, Length),
-}
-
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Deserialize, Debug)]
 pub enum Layout {
-    Absolute(AbsolutePosition, AbsoluteSize),
+    FromCenter(Point, Size),
+    FromTopLeft(Point, Point),
     Grid(GridSize, GridPosition),
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct GaugeConfig {
+#[derive(Deserialize, Debug)]
+pub struct Gauge {
     name: String,
     kind: GaugeType,
-    source: ChannelName,
+    channel: String,
     layout: Layout,
-    style: Option<Style>
+    styles: Vec<(State, String)>
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct ChannelConfig {
-    index: u32,
+#[derive(Deserialize, Debug)]
+pub struct Channel {
     name: String,
-    scale: Interval,
-    warning: Option<Interval>,
-    danger: Option<Interval>,
-    units: Option<Unit>,
-    convert: Option<Polynomial>,
+    source: Source,
+    units: Unit,
+    transform: Conversion
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub enum DataSource {
-    Mock,
-    RaceCapturePro(String)
-}
+#[derive(Deserialize, Debug)]
+pub struct Page(Vec<String>);
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Config {
+#[derive(Deserialize, Debug)]
+pub struct V1 {
     width: u32,
     height: u32,
-    gauges: Vec<GaugeConfig>,
-    channels: Vec<ChannelConfig>,
-    datasource: DataSource,
-    style: Style
+    channels: Vec<Channel>,
+    conditions: Vec<When>,
+    gauges: Vec<Gauge>,
+    pages: Vec<Page>,
+    styles: Vec<Style>
 }
 
+#[derive(Debug)]
+pub enum V1Error {
+    ReadError(String),
+    ParseError(String),
+    NoSuchChannel(String),
+    NoSuchState(State),
+    NoSuchGauge(Gauge)
+}
 
+impl V1 {
+    pub fn new_from_file(path: String) -> Result<V1, V1Error> {
+        let reader = File::open(path).expect("Couldn't open config");
+        let config: V1 = from_reader(reader).unwrap();
+        config.validate()
+    }
+
+    fn validate(self) -> Result<V1, V1Error> {
+        // check all channels, states, and gauges have unique names
+        // check that all condition tests are based on defined values
+        // check that condition graph is acyclic
+        // check that all gauges use a defined channel
+        // check that all states within a gauge are mutually exclusive
+        // warn about unused channels
+        // warn about unused states
+        // warn about about overlapping gauges
+        Ok(self)
+    }
+}
 
 fn main() {
-    let file = File::open("config.ron").expect("couldn't open config");
-    let config: Config = from_reader(file).unwrap();
-    println!("Config: {:?}", config);
+    println!("Config: {:?}", V1::new_from_file("config.ron".to_string()));
 }
