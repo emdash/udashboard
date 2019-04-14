423bacb2ffd1b2755f0f2f772ad5767bd11818a3
Brandon Lewis <dotsony@gmail.com>
2019-04-13 17:42:15 -0700

    Incremental progress

--- src/render.rs
+++ src/render.rs
@@ -8,14 +8,15 @@ use cairo::{Context, Format, ImageSurface};
 use crate::config;
 use crate::config::{
     Bounds,
+    Color,
     Config,
     Divisions,
     Gauge,
     GaugeStyle,
     GaugeType,
     Lamp,
+    Pattern,
     Scale,
-    Color,
     Screen,
     Style,
     StyleSet,
@@ -24,73 +25,10 @@ use crate::config::{
 
 use crate::data::State;
 
-fn pattern_from_color(color: Color) -> cairo::Pattern {
-    let ret = cairo::SolidPattern::from_rgba(
-        color.0.into(),
-        color.1.into(),
-        color.2.into(),
-        color.3.into()
-    );
-
-    cairo::Pattern::SolidPattern(ret)
-}
-
-#[derive(Debug)]
-enum Pattern {
-    Hidden,
-    Solid(cairo::Pattern),
-    SlowBlink(cairo::Pattern),
-    FastBlink(cairo::Pattern)
-}
-
-impl Pattern {
-    pub fn new(config: config::Pattern) -> Pattern {
-        match config {
-            config::Pattern::Hidden => Pattern::Hidden,
-            config::Pattern::Solid(x) => {
-                Pattern::Solid(pattern_from_color(x))
-            },
-            config::Pattern::SlowBlink(x) =>
-                Pattern::SlowBlink(pattern_from_color(x)),
-            config::Pattern::FastBlink(x) =>
-                Pattern::FastBlink(pattern_from_color(x))
-        }
-    }
-
-    pub fn set_source(
-        &self,
-        cr: &Context,
-        null: &cairo::Pattern,
-        time: u64
-    ) {
-        let slow_blink = (time / 1000) % 2 == 1;
-        let fast_blink = (time / 250) % 2 == 1;
-        match self {
-            Pattern::Hidden => cr.set_source(null.into()),
-            Pattern::Solid(x) => {
-                cr.set_source(&x)
-            },
-            Pattern::SlowBlink(x) => if slow_blink {
-                cr.set_source(&x);
-            } else {
-                cr.set_source(null);
-            },
-            Pattern::FastBlink(x) => if fast_blink {
-                cr.set_source(&x);
-            } else {
-                cr.set_source(null);
-            }
-        }
-    }
-}
-
 pub struct CairoRenderer {
     screen: Screen,
     pages: Vec<Vec<Gauge>>,
-    background: Pattern,
-    foreground: Pattern,
-    indicator: Pattern,
-    null: cairo::Pattern,
+    default_style: Style,
     page: usize
 }
 
@@ -103,12 +41,7 @@ impl CairoRenderer {
         CairoRenderer {
             screen: screen,
             pages: pages,
-            background: Pattern::new(default_style.background),
-            foreground: Pattern::new(default_style.foreground),
-            indicator: Pattern::new(default_style.indicator),
-            null: cairo::Pattern::SolidPattern(
-                cairo::SolidPattern::from_rgba(0.0, 0.0, 0.0, 0.0)
-            ),
+            default_style: default_style,
             page: 0
         }
     }
@@ -118,11 +51,33 @@ impl CairoRenderer {
         cr: &Context,
         state: &State
     ) {
-        self.background.set_source(cr, &self.null, state.time);
+        cr.save();
+
+        self.set_pattern(cr, self.default_style.background);
         cr.paint();
 
         for gauge in &self.pages[self.page] {
             self.render_gauge(cr, &gauge, state);
+        }
+
+        cr.restore();
+    }
+
+    fn set_color(&self, cr: &Context, color: Color) {
+        cr.set_source_rgba(
+            color.0.into(),
+            color.1.into(),
+            color.2.into(),
+            color.3.into()
+        );
+    }
+
+    fn set_pattern(&self, cr: &Context, pat: Pattern) {
+        match pat {
+            Pattern::Hidden => cr.set_source_rgba(0.0, 0.0, 0.0, 0.0),
+            Pattern::Solid(c) => self.set_color(cr, c),
+            Pattern::SlowBlink(c) => self.set_color(cr, c),
+            Pattern::FastBlink(c) => self.set_color(cr, c)
         }
     }
 
@@ -132,16 +87,10 @@ impl CairoRenderer {
         gauge: &Gauge,
         state: &State
     ) {
-        let label = &gauge.label;
         let kind = &gauge.kind;
-        let value = state.values.get(&gauge.channel);
-        let bounds = &gauge.bounds;
-        // XXX: get style
-
-        println!("{:?}", bounds);
 
         match kind {
-            GaugeType::Dial(opts) => self.dial(cr, bounds, opts, label, value),
+            GaugeType::Dial(opts) => self.dial(cr, gauge, opts, state),
             GaugeType::VerticalBar(opts) => {println!("{:?}", opts)}
             GaugeType::HorizontalBar(opts) => {println!("{:?}", opts)}
             GaugeType::VerticalWedge(opts) => {println!("{:?}", opts)}
@@ -154,18 +103,18 @@ impl CairoRenderer {
     fn dial(
         &self,
         cr: &Context,
-        bounds: &Bounds,
+        gauge: &Gauge,
         scale: &Scale,
-        label: &String,
-        value: Option<&f32>
+        state: &State
     ) {
         cr.save();
 
+        let bounds = gauge.bounds;
         let radius = (bounds.width.min(bounds.height) / 2.0) as f64;
         let cx = (bounds.x + bounds.width / 2.0) as f64;
         let cy = (bounds.y + bounds.height / 2.0) as f64;
 
-        cr.set_source_rgb(1.0, 0.0, 0.0);
+        self.set_pattern(cr, self.default_style.foreground);
         cr.arc(cx, cy, radius, 0.0, 2.0 * PI);
 
         match scale.3 {
