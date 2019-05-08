bb88c5466cc50e7be05b098fbdf4fe4615bdd673
Brandon Lewis <dotsony@gmail.com>
2019-05-07 20:38:53 -0700

    Fix up examples.

---
+++ examples/demo.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+#
+# Run me from project root: ./examples/demo.sh
+
+python scripts/simulate.py \
+       --sin "RPM"                0   6500      \
+       --sin "ECT"                100 230       \
+       --sin "OIL_PRESSURE"       45  60        \
+       --identity "SESSION_TIME" | cargo run examples/demo.ron $1
