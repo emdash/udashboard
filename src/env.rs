b863f07b17fa7bf3fec650ee2c91758d3849d0f1
Brandon Lewis <dotsony@gmail.com>
2020-03-08 00:04:52 -0800

    All the typecheck machinery is in place

--- src/env.rs
+++ src/env.rs
@@ -1,57 +1,50 @@
 use crate::ast::{Node, AList, Map};
+use std::cell::RefCell;
 use std::fmt::Debug;
 
 
 // Datastructure to manage lexical scoping.
 pub struct Env<T> {
-    stack: Vec<Map<T>>
+    scope: RefCell<Map<T>>,
+    parent: Option<Node<Env<T>>>
 }
 
 
 impl<T> Env<T> where T: Clone + Debug {
-    pub fn new() -> Env<T> {
-        let mut ret = Env {stack: Vec::new()};
-        ret.begin();
+    fn new(parent: Option<Node<Env<T>>>) -> Env<T> {
+        let scope = RefCell::new(Map::new());
+        Env {scope, parent}
+    }
+
+    pub fn root() -> Env<T> {
+        Self::new(None)
+    }
+
+    pub fn chain(parent: &Node<Env<T>>) -> Env<T> {
+        let mut ret = Self::new(Some(parent.clone()));
         ret
     }
 
     // Look up an identifier from anywhere in our scope chain.
     pub fn get(&self, key: &String) -> Option<Node<T>> {
-        println!("{:?}", self.stack);
-        let len = self.stack.len();
-        for i in 0..len {
-            let idx = len - i - 1;
-            println!("{:?}", idx);
-            let env = &self.stack[idx];
-            if let Some(value) = env.get(key) {
-                return Some(value.clone())
-            }
+        if let Some(value) = self.scope.borrow().get(key) {
+            Some(value.clone())
+        } else if let Some(env) = &self.parent {
+            env.get(key)
+        } else {
+            None
         }
-        None
     }
 
     // Insert a value in the current scope.
-    pub fn define(&mut self, key: &String, value: &Node<T>) {
-        let env = self.stack.last_mut().unwrap();
-        let key = key.clone();
-        let value = value.clone();
-        env.insert(key, value);
+    pub fn define(&self, key: &String, value: &Node<T>) {
+        self.scope.borrow_mut().insert(key.clone(), value.clone());
     }
 
     // Import the map of values into the current scope.
-    pub fn import(&mut self, scope: &AList<T>) {
+    pub fn import(&self, scope: &AList<T>) {
         for (k, v) in scope.iter() {
             self.define(k, v)
         }
-    }
-
-    // Begin a new scope.
-    pub fn begin(&mut self) {
-        self.stack.push(Map::new())
-    }
-
-    // End the current scope.
-    pub fn end(&mut self) {
-        self.stack.pop();
     }
 }
