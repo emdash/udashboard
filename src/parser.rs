9876d11903e66aabe85aba5d3a936e2a9ca2fb68
Brandon Lewis <dotsony@gmail.com>
2020-04-02 10:17:56 -0700

    Add some parse tests.

--- src/parser.rs
+++ src/parser.rs
@@ -1,11 +1,10 @@
-
-
-
 #[cfg(test)]
 mod tests {
     use crate::grammar;
+    use crate::ast;
     use crate::ast::*;
-    use BinOp::*;
+    use Expr::*;
+    use ast::BinOp::*;
 
     fn assert_parses_to(text: &'static str, ast: Expr) {
         assert_eq!(
@@ -15,44 +14,88 @@ mod tests {
     }
 
     #[test]
+    fn test_basic() {
+        assert_parses_to("3 + 4 * 5", bin(
+            Add,
+            Int(3),
+            bin(Mul, Int(4), Int(5))
+        ));
+        assert_parses_to("a + 3", bin(Add, Id(String::from("a")), Int(3)));
+        assert_parses_to("3 * a", bin(Mul, Int(3), id("a")));
+    }
+
+    #[test]
     fn test_terms() {
-        assert_parses_to("42", Expr::Int(42));
-        assert_parses_to("42.0", Expr::Float(42.0));
-        assert_parses_to("(42)", Expr::Int(42));
-        assert_parses_to("foo", Expr::Id(String::from("foo")));
+        assert_parses_to("42", Int(42));
+        assert_parses_to("42.0", Float(42.0));
+        assert_parses_to("(42)", Int(42));
+        assert_parses_to("foo", id("foo"));
     }
 
     #[test]
     fn test_relational() {
         assert_parses_to("3 + 4 < 3 * 4", bin(
             Lt,
-            bin(Add, Expr::Int(3), Expr::Int(4)),
-            bin(Mul, Expr::Int(3), Expr::Int(4))
+            bin(Add, Int(3), Int(4)),
+            bin(Mul, Int(3), Int(4))
         ));
 
         assert_parses_to("3 + 4 > 3 * 4", bin(
             Gt,
-            bin(Add, Expr::Int(3), Expr::Int(4)),
-            bin(Mul, Expr::Int(3), Expr::Int(4))
+            bin(Add, Int(3), Int(4)),
+            bin(Mul, Int(3), Int(4))
         ));
 
         assert_parses_to("3 + 4 <= 3 * 4", bin(
             Lte,
-            bin(Add, Expr::Int(3), Expr::Int(4)),
-            bin(Mul, Expr::Int(3), Expr::Int(4))
+            bin(Add, Int(3), Int(4)),
+            bin(Mul, Int(3), Int(4))
         ));
 
         assert_parses_to("3 + 4 >= 3 * 4", bin(
             Gte,
-            bin(Add, Expr::Int(3), Expr::Int(4)),
-            bin(Mul, Expr::Int(3), Expr::Int(4))
+            bin(Add, Int(3), Int(4)),
+            bin(Mul, Int(3), Int(4))
         ));
 
         assert_parses_to("3 + 4 == 3 * 4", bin(
             Eq,
-            bin(Add, Expr::Int(3), Expr::Int(4)),
-            bin(Mul, Expr::Int(3), Expr::Int(4))
+            bin(Add, Int(3), Int(4)),
+            bin(Mul, Int(3), Int(4))
         ));
+    }
+
+    #[test]
+    fn test_logic() {
+        assert_parses_to(
+            "x >= lower and x <= upper",
+            bin(And,
+                bin(Gte, id("x"), id("lower")),
+                bin(Lte, id("x"), id("upper"))));
+
+        assert_parses_to(
+            "x >= 3 or x > 4 and x > 5",
+            bin(And,
+                bin(Or,
+                    bin(Gte, id("x"), Int(3)),
+                    bin(Gt, id("x"), Int(4))),
+                bin(Gt, id("x"), Int(5))));
+
+        assert_parses_to(
+            "x >= 3 or (x > 4 and x > 5)",
+            bin(Or,
+                bin(Gte, id("x"), Int(3)),
+                bin(And,
+                    bin(Gt, id("x"), Int(4)),
+                    bin(Gt, id("x"), Int(5)))));
+
+        assert_parses_to(
+            "(x >= 3) or (x > 4 and x > 5)",
+            bin(Or,
+                bin(Gte, id("x"), Int(3)),
+                bin(And,
+                    bin(Gt, id("x"), Int(4)),
+                    bin(Gt, id("x"), Int(5)))));
     }
 }
 
