df08716aeba6e75a11fc43433fe26e7079a332cc
Brandon Lewis <dotsony@gmail.com>
2020-04-03 16:56:00 -0700

    Implement statements, with tests.

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,6 @@
+
+
+
 #[cfg(test)]
 mod tests {
     use crate::grammar;
@@ -10,6 +13,13 @@ mod tests {
     fn assert_parses_to(text: &'static str, ast: Expr) {
         assert_eq!(
             grammar::ExprParser::new().parse(text).unwrap(),
+            ast
+        );
+    }
+
+    fn assert_statement(text: &'static str, ast: Statement) {
+        assert_eq!(
+            grammar::StatementParser::new().parse(text).unwrap(),
             ast
         );
     }
@@ -238,6 +248,180 @@ mod tests {
         assert_parses_to(
             "(foo()).bar",
             dot(call(id("foo"), vec!{}), "bar")
+        );
+    }
+
+    #[test]
+    fn test_simple_statement() {
+        assert_statement("fill <- ;", emit("fill", vec!{}));
+        assert_statement(
+            "moveto <- x, y;",
+            emit("moveto", vec!{id("x"), id("y")})
+        );
+
+        assert_statement(
+            "let y = x * 3 + 4;",
+            def("y", bin(Add, bin(Mul, id("x"), Int(3)), Int(4)))
+        );
+    }
+
+    #[test]
+    fn test_block() {
+        assert_statement("{}", statement_block(vec!{}));
+        assert_statement(
+            "{let x = 1; let y = 2; moveto <- x, y;}",
+            statement_block(vec!{
+                def("x", Int(1)),
+                def("y", Int(2)),
+                emit("moveto", vec!{ id("x"), id("y")})
+            })
+        );
+
+        assert_statement(
+            "{{} {{let x = 1; let y = 2;}}}",
+            statement_block(vec!{
+                statement_block(vec!{}),
+                statement_block(vec!{def("x", Int(1)), def("y", Int(2))})
+            })
+        );
+
+        assert_statement(
+            "{{{{{let y = 1;}}}}}",
+            def("y", Int(1))
+        );
+    }
+
+    #[test]
+    fn test_list_iter() {
+        let test = r#"{
+              let x = [1, 2, 3];
+              for i in x {
+                  moveto <- i, i;
+                  circle <- 50.0;
+              }
+
+        }"#;
+
+        assert_statement(test, statement_block(vec!{
+            def("x", list(vec!{Int(1), Int(2), Int(3)})),
+            list_iter("i", id("x"), statement_block(vec!{
+                emit("moveto", vec!{id("i"), id("i")}),
+                emit("circle", vec!{Float(50.0)})
+            }))
+        }));
+    }
+
+
+    #[test]
+    fn test_map_iter() {
+        let test = r#"{
+              let x = {"a": 1, "b": 2, "c": 3};
+              for (k, v) in x {
+                  moveto <- v, v;
+                  text <- k;
+              }
+
+        }"#;
+
+        assert_statement(test, statement_block(vec!{
+            def("x", map(vec!{
+                (s("a"), Int(1)),
+                (s("b"), Int(2)),
+                (s("c"), Int(3))
+            })),
+            map_iter("k", "v", id("x"), statement_block(vec!{
+                emit("moveto", vec!{id("v"), id("v")}),
+                emit("text", vec!{id("k")})
+            }))
+        }));
+    }
+
+    #[test]
+    fn test_guard() {
+        assert_statement(
+            "if a { text <- b; }",
+            guard(
+                vec!{(id("a"), emit("text", vec!{id("b")}))},
+                None
+            )
+        );
+
+        assert_statement(
+            r#"if a { text <- b; } else { text <- "error"; }"#,
+            guard(
+                vec!{(id("a"), emit("text", vec!{id("b")}))},
+                Some(emit("text", vec!{string("error")}))
+            )
+        );
+
+        assert_statement(
+            r#"if a {
+               text <- "a";
+            } elif b {
+               text <- "b";
+            } else {
+               text <- "error";
+            }"#,
+            guard(
+                vec!{
+                    (id("a"), emit("text", vec!{string("a")})),
+                    (id("b"), emit("text", vec!{string("b")})),
+                },
+                Some(emit("text", vec!{string("error")}))
+            )
+        );
+
+        assert_statement(
+            r#"if a {
+               text <- "a";
+            } elif b {
+               text <- "b";
+            } elif c {
+               text <- "c";
+            } else {
+               text <- "error";
+            }"#,
+            guard(
+                vec!{
+                    (id("a"), emit("text", vec!{string("a")})),
+                    (id("b"), emit("text", vec!{string("b")})),
+                    (id("c"), emit("text", vec!{string("c")})),
+                },
+                Some(emit("text", vec!{string("error")}))
+            )
+        );
+
+        assert_statement(
+            r#"if a {
+               text <- "a";
+            } elif b {
+               text <- "b";
+            }"#,
+            guard(
+                vec!{
+                    (id("a"), emit("text", vec!{string("a")})),
+                    (id("b"), emit("text", vec!{string("b")})),
+                },
+                None
+            )
+        );
+
+        assert_statement(
+            r#"if a {
+               text <- "a";
+            } elif b {
+               text <- "b";
+            } elif c {
+               text <- "c";
+            }"#,
+            guard(
+                vec!{
+                    (id("a"), emit("text", vec!{string("a")})),
+                    (id("b"), emit("text", vec!{string("b")})),
+                    (id("c"), emit("text", vec!{string("c")})),
+                },
+                None
+            )
         );
     }
 }
