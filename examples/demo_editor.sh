ce820a7e2fd957d4bd460a6bee47c8f2e6fa50a2
Brandon Lewis <dotsony@gmail.com>
2020-07-08 19:57:32 -0700

    Add new examples

---
+++ examples/demo_editor.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+#
+# Run me from project root: ./examples/demo.sh
+
+python scripts/simulate.py \
+       --sin "RPM"                0   6500      \
+       --sin "ECT"                100 230       \
+       --sin "OIL_PRESSURE"       45  60        \
+       --identity "SESSION_TIME" | python3 scripts/editor.py examples/tach.img
